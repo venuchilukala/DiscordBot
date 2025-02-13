796626da6ecaf1bc4a8443c326e9e301a54d69bf
venuchilukala <venuchilukala07@gmail.com>
2025-02-13 13:31:02 +0530

    command created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits, InteractionCallback } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,10 +7,24 @@ dotenv.config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on("messageCreate", (message) => {
+    // console.log(message)
     if(message.author.bot) return;
+
+    if(message.content.startsWith("create")){
+        const url = message.content.split('create')[1]
+        return message.reply({
+            content : "Generating Short ID for " + url,
+        })
+    }
+
     message.reply({
-        content : `Hello ${message.author.globalName}`
+        content : `Hi ${message.author.globalName}`
     })
 })
 
-client.login(process.env.DISCORD_TOKEN)
+client.on("interactionCreate", interaction => {
+    // console.log(interaction)
+    interaction.reply("Pong 😉")
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
